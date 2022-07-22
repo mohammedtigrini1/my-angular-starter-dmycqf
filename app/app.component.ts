b557d482765013c2389351c524e4542a28009c85
mohammedtigrini1 <mohammed.tigrini@appdirect.com>
2022-07-22 18:10:51 -0400

    1 file modified, 1 added

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,9 +1,20 @@
-import { Component, VERSION } from '@angular/core';
+import { Component } from '@angular/core';
+import { ElementRef, NgModule, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
-export class AppComponent  {
-  version = 'Angular: v' + VERSION.full;
+export class AppComponent {
+  // its important myCanvas matches the variable name in the template
+  @ViewChild('myCanvas')
+  myCanvas: ElementRef<HTMLCanvasElement>;
+
+  public context: CanvasRenderingContext2D;
+
+  ngAfterViewInit(): void {
+    this.context = this.myCanvas.nativeElement.getContext('2d');
+    this.context.fillStyle = '#FF0000';
+    this.context.fillRect(0, 0, 150, 75);
+  }
 }
